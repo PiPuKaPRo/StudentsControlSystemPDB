c9b082e5711801e108a3a2643f0955bf6e1012f9
Evgeniy Sakov <91192469+PiPuKaPRo@users.noreply.github.com>
2023-11-16 16:45:30 +0300

    Add files via upload

---
+++ StudentsController/src/test/java/ru/vsu/cs/sakov/StudentsController/StudentsControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vsu.cs.sakov.StudentsController;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentsControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
